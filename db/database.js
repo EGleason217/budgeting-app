29abc374111f96f1064f1cf2cd66727804bc8312
Erik J Gleason <erik@gleasoncf.com>
2025-06-14 21:30:37 -0400

    Update database.js

--- db/database.js
+++ db/database.js
@@ -1,8 +1,8 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./budget.db');
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("./budget.db");
 
 db.serialize(() => {
-db.run(`
+  db.run(`
   CREATE TABLE IF NOT EXISTS entries (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     accountId INTEGER,
@@ -16,6 +16,13 @@ db.run(`
     FOREIGN KEY(accountId) REFERENCES accounts(id)
   )
 `);
+  db.run(`
+  CREATE TABLE IF NOT EXISTS accounts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    initialBalance REAL NOT NULL
+  )
+`);
 });
 
-module.exports = db; 
+module.exports = db;
